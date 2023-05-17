db1ef107650467ac72c0b4321cb1c5139bd8029a
Coconut <coconutat@protonmail.com>
2023-05-17 12:15:32 +0800

    Fix Build Script

--- build_Kernel.sh
+++ build_Kernel.sh
@@ -33,24 +33,30 @@ echo " "
 echo "Setting EXTRAVERSION"
 export EV=EXTRAVERSION=_KSU_V$v
 
-#构建P10内核部分
+#构建hi6250内核部分
 echo "***Building for hi6250 version...***"
 make ARCH=arm64 O=out $EV merge_hi6250_KSU_defconfig
 # 定义编译线程数
 make ARCH=arm64 O=out $EV -j256 2>&1 | tee log-${date}.txt
 
-#打包P10版内核
+#打包hi6250版内核
 
 if [ -f out/arch/arm64/boot/Image.gz ];
 then
 	echo "***Packing hi6250 version kernel...***"
 	cp out/arch/arm64/boot/Image.gz Image.gz 
 	cp out/arch/arm64/boot/Image.gz tools/AnyKernel2/Image.gz
+	cp out/arch/arm64/boot/Image.gz tools/AnyKernel3/Image.gz
 	cd tools/AnyKernel2
 	zip -r9 hi6250_KSU_V"$v"-${date}.zip * > /dev/null
 	cd ../..
 	mv tools/AnyKernel2/hi6250_KSU_V"$v"-${date}.zip hi6250_KSU_V"$v"-${date}.zip
 	rm -rf tools/AnyKernel2/Image.gz
+	cd tools/AnyKernel3
+	zip -r9 hi6250_KSU_V"$v"-${date}_AK3.zip * > /dev/null
+	cd ../..
+	mv tools/AnyKernel3/hi6250_KSU_V"$v"-${date}_AK3.zip hi6250_KSU_V"$v"-${date}_AK3.zip
+	rm -rf tools/AnyKernel3/Image.gz
 	echo " "
 	echo "***Sucessfully built hi6250 version kernel...***"
 	echo " "
